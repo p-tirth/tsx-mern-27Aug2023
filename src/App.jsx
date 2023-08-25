5f10b89e5eb7bcb87b7d0e90f8c3b218efabae4d
Tirth <tirthpatel2992003@gamail.com>
2023-08-26 03:34:32 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import CharecterList from "./components/CharecterList"
+import Navbar from "./components/Navbar"
 
 export default function App() {
   return (
     <>
-      <div className="flex justify-center items-center">
+      <div className="">
+        <Navbar/>
+        <div className="flex justify-center items-center">
         <CharecterList/>
+        </div>
       </div>
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import logo from "../assets/starWars.png" 
 
-export default function Navbar() {
+const Navbar = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <nav className="flex items-center justify-between p-4 bg-black">
+      {/* Logo */}
+      <div className="flex items-center">
+        <img
+          src={logo}
+          alt="Logo"
+          className="h-20 mr-4"
+        />
+      </div>
+
+      {/* Search Input */}
+      <div className="flex items-center">
+        <input
+          type="text"
+          placeholder="Search characters..."
+          className="px-4 py-2 mr-4 border rounded-md"
+        />
+      </div>
+
+      {/* Filter Options */}
+      <div className="flex items-center">
+        <select className="px-4 py-2 border rounded-md">
+          <option value="vehicles">Vehicles</option>
+          <option value="starships">Starships</option>
+          <option value="species">Species</option>
+          <option value="planets">Planets</option>
+          <option value="films">Films</option>
+        </select>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
