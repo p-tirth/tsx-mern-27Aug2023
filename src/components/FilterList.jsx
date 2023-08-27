e6c76f5a70a70d3559c3507c6e3ecf205fe7c82e
Tirth <tirthpatel2992003@gamail.com>
2023-08-27 19:52:26 +0530

    UPDATE empty filter

--- src/components/FilterList.jsx
+++ src/components/FilterList.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const FilterList = () => {
-  const [checkboxes, setCheckboxes] = useState(['vehicles', 'starships', 'species', 'planets', 'people', 'films']);
+  const [checkboxes, setCheckboxes] = useState([]);
   const checkboxOptions = ['vehicles', 'starships', 'species', 'planets', 'people', 'films'];
 
   const handleCheckboxChange = (option) => {
